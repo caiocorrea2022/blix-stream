a3dcbf969140ab633b44bfff72ce3d9e0ea8a89b
Roberto Muniz <robertotmuniz@gmail.com>
2022-07-07 04:11:04 -0300

    about=

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { HeaderLeftSide, HeaderRightSide, Avatar, Row, Avatar2} from "./style";
+import { HeaderLeftSide, HeaderRightSide, Avatar, Row, Avatar2 } from "./style";
 import { Icon } from "react-native-elements";
 import THEME from "../../config/theme";
 import { HeaderContainer, StandardText } from "../../config/theme/globalStyles";
-import { DrawerActions } from '@react-navigation/native';
-import TouchableText from '../../components/TouchableText'
+import { DrawerActions } from "@react-navigation/native";
+import TouchableText from "../../components/TouchableText";
 import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useAuth } from "../../context/useAuth";
 
-const Header = ({ onPress, about }) => {
+const Header = ({ onPress, about, signUp }) => {
   const navigation = useNavigation();
   const { user } = useAuth();
 
-  return (
-    about ? (
-      <HeaderContainer>
-        <HeaderLeftSide>
-          <Avatar
-            resizeMode="contain"
-            source={require("../../assets/LogoAbout.png")}
-          />
-        </HeaderLeftSide>
-        <HeaderRightSide
-        style={{alignItems: "center"}}
-        >
+  return about ? (
+    <HeaderContainer>
+      <HeaderLeftSide>
+        <Avatar
+          resizeMode="contain"
+          source={require("../../assets/LogoAbout.png")}
+        />
+      </HeaderLeftSide>
+      {signUp ? (
+        <HeaderRightSide style={{ alignItems: "center" }}></HeaderRightSide>
+      ) : (
+        <HeaderRightSide style={{ alignItems: "center" }}>
           {user ? (
             <StandardText
               color={THEME.COLORS.ICON_HEADER_OVERPHOTO}
@@ -35,51 +35,64 @@ const Header = ({ onPress, about }) => {
             </StandardText>
           ) : (
             <>
-            <StandardText
-            color={THEME.COLORS.ICON_HEADER_OVERPHOTO}
-            margin="0rem 1rem"
-            onPress={onPress}
-          >
-            VER APLICATIVO
-          </StandardText>
-            <StandardText
-              color={THEME.COLORS.ICON_HEADER_OVERPHOTO}
-              margin="0rem 1rem"
-              onPress={() => navigation.navigate("Login")}
-            >
-              LOGIN
-            </StandardText>
+              <StandardText
+                color={THEME.COLORS.ICON_HEADER_OVERPHOTO}
+                margin="0rem 1rem"
+                onPress={onPress}
+              >
+                VER APLICATIVO
+              </StandardText>
+              <StandardText
+                color={THEME.COLORS.ICON_HEADER_OVERPHOTO}
+                margin="0rem 1rem"
+                onPress={() => navigation.navigate("Login")}
+              >
+                LOGIN
+              </StandardText>
             </>
           )}
         </HeaderRightSide>
-      </HeaderContainer>
-    ) : (
-      <HeaderContainer>
-        <HeaderLeftSide>
-          {onPress ? (
-            <></>
-          ) : (
+      )}
+    </HeaderContainer>
+  ) : (
+    <HeaderContainer>
+      <HeaderLeftSide>
+        {onPress ? (
+          <Row>
             <Icon
               type="material-community"
-              name="menu"
-              color={THEME.COLORS.ICON_HEADER_OVERPHOTO}
-              size={34}
-              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+              name="chevron-left"
+              color={THEME.COLORS.ICON_HEADER}
+              size={THEME.FONTSIZE.BIG}
+              onPress={onPress}
             />
-          )}
-        </HeaderLeftSide>
+            <TouchableText
+              onPress={onPress}
+              title={"Voltar"}
+              color={THEME.COLORS.ICON_HEADER}
+            ></TouchableText>
+          </Row>
+        ) : (
+          <Icon
+            type="material-community"
+            name="menu"
+            color={THEME.COLORS.ICON_HEADER_OVERPHOTO}
+            size={34}
+            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+          />
+        )}
+      </HeaderLeftSide>
 
-        <HeaderRightSide>
-          <TouchableOpacity onPress={() => navigation.navigate('About')}>
-            <Avatar2 resizeMode="contain" source={require("../../assets/LogoMain.png")} />
-          </TouchableOpacity>
-        </HeaderRightSide>
-      </HeaderContainer>
-    )
+      <HeaderRightSide>
+        <TouchableOpacity onPress={() => navigation.navigate("About")}>
+          <Avatar2
+            resizeMode="contain"
+            source={require("../../assets/LogoMain.png")}
+          />
+        </TouchableOpacity>
+      </HeaderRightSide>
+    </HeaderContainer>
   );
 };
 
 export default Header;
-
-
-
