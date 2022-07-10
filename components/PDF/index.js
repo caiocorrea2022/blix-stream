14ccb481ceb4e6f72682986d8a84e9361b735316
rodrigo.tejada <rogtejada@hotmail.com>
2022-07-10 16:12:15 -0300

    testing pdf

--- components/PDF/index.js
+++ components/PDF/index.js
@@ -3,7 +3,7 @@ import { Container } from './style';
 
 export default function PDF({pdf}) {
   return (
-    <Container>
+    <Container style={{flex: 1}}>
        <iframe src={pdf} height={window.innerHeight} width="100%" />
     </Container>
   )};
